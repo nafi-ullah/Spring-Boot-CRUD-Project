5e414212ca00c9b6f7640821c22f6b2346f82269
nafi-ullah <shafinnafiullah@gmail.com>
2023-11-03 18:17:54 +0600

    json format add

--- src/main/java/com/springlearn/Main.java
+++ src/main/java/com/springlearn/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication //declare it as spring boot application
 @RestController
 public class Main {
@@ -14,8 +16,21 @@ public class Main {
 
     @GetMapping("/")
     public GreetResponse greet(){
-        return new GreetResponse("Hello");
+        return new GreetResponse("Hello",
+                List.of("Java","Cpp","JavaScript"),
+                new Person("Emran")
+                );
     }
 
-    record GreetResponse(String greet){}
+    record Person(String name){
+
+    }
+
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+            ){
+
+    }
 }
