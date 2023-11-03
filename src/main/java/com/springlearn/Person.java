76cadecb673b774c8caca681155d8fa6d4877495
nafi-ullah <shafinnafiullah@gmail.com>
2023-11-03 21:41:48 +0600

    entity added

--- src/main/java/com/springlearn/Person.java
+++ src/main/java/com/springlearn/Person.java
@@ -1,8 +1,21 @@
 package com.springlearn;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
 public class Person {
+    @Id
+    @SequenceGenerator(
+            name = "person_id_sequence",
+            sequenceName = "person_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+
     private Integer id;
     private String name;
     private String email;
