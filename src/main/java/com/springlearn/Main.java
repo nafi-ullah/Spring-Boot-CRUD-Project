19a327e181909722ca91d708aed677e3b9c3730a
nafi-ullah <shafinnafiullah@gmail.com>
2023-11-04 12:50:42 +0600

    get from api

--- src/main/java/com/springlearn/Main.java
+++ src/main/java/com/springlearn/Main.java
@@ -3,34 +3,28 @@ package com.springlearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication //declare it as spring boot application
 @RestController
+@RequestMapping("api/v1/persons")
 public class Main {
+
+    private final PersonRepository personRepository;
+
+    public Main(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     public static void main(String[] args){
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/")
-    public GreetResponse greet(){
-        return new GreetResponse("Hello",
-                List.of("Java","Cpp","JavaScript"),
-                new Person("Emran")
-                );
-    }
-
-    record Person(String name){
-
-    }
-
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
-            ){
-
-    }
+   @GetMapping
+    public List<Person> getPerson(){
+        return personRepository.findAll();
+   }
 }
